ae53f91790336f4bd9dbd16179fa98736ea5eb02
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2025-04-11 20:35:37 +0200

    feat: load environment variables and configure database settings for development and production

--- flipnote_quizzer/settings/development.py
+++ flipnote_quizzer/settings/development.py
@@ -1,3 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 DEBUG = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8080",
@@ -5,3 +9,14 @@ CSRF_TRUSTED_ORIGINS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
 ]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
--- flipnote_quizzer/settings/production.py
+++ flipnote_quizzer/settings/production.py
@@ -1,5 +1,22 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 DEBUG = False
 
 CORS_ALLOWED_ORIGINS = [
     "https://flipnote-quizzer-pro.lovable.app/",
 ]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "OPTIONS": {"sslmode": "require"},
+    }
+}
