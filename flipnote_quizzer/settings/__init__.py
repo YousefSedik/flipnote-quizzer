5ebeff32b4ad15d1bf4f127fac24fc04be2d9105
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2025-04-18 23:43:03 +0200

    fix: set default environment to production and remove unnecessary else statement

--- flipnote_quizzer/settings/__init__.py
+++ flipnote_quizzer/settings/__init__.py
@@ -3,10 +3,8 @@ import os
 
 dotenv.load_dotenv()
 
-ENVIRONMENT = os.getenv("ENVIRONMENT", "").strip().lower()
+ENVIRONMENT = os.getenv("ENVIRONMENT", "production").strip().lower()
 if ENVIRONMENT == "production":
     from .production import *
 elif ENVIRONMENT == "development":
     from .development import *
-else:
-    print("ensure that ENVIRONMENT is set to development OR production")
